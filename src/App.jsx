ec6c9596b6968dde5de8d2483142e87bc79a15da
Daniel Corralejo <daniel.corralejo1988@gmail.com>
2023-06-14 19:33:20 -0500

    Modify hopepage h1 title

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { API_URL } from "../env"
 function App() {
   return (
     <>
-      <h1>Hola Mundo { API_URL }</h1>
+      <h1>Hola Mundo esta es la rama de desarrollo</h1>
     </>
   )
 }
