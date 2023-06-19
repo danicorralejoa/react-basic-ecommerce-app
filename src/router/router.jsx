583e79b444ec4f9ec22461df63dbfd29ab6d1749
Daniel Corralejo <daniel.corralejo1988@gmail.com>
2023-06-19 10:53:05 -0500

    Fix casing issue with App.jsx impport in router.jsx component

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Error404 from "../components/pages/Error404.jsx";
-import App from "../components/templates/app.jsx";
+import App from "../components/templates/App.jsx";
 import Products from "../components/pages/Products.jsx";
 import Homepage from "../components/pages/Homepage.jsx";
 
