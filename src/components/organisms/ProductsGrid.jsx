c5a3f1506b167483ad7aca023c7bd0461eae3834
Daniel Corralejo <daniel.corralejo1988@gmail.com>
2023-09-26 19:03:37 -0500

    Cart Feature: Add CTA functionality on ProductGridd to 'Add to Cart' and 'Remove From Cart'

--- src/components/organisms/ProductsGrid.jsx
+++ src/components/organisms/ProductsGrid.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { currencyFormatter } from "../../helpers/dataTransformations";
+import { CartContext } from "../../context/CartContext";
 
 const ProductsGrid = ({ product }) => {
   const { id, images, title, price, description } = product;
+  const { state, dispatch } = useContext(CartContext);
+
+  const addToCart = () => {
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: product,
+    });
+  };
+
+  const removeFromCart = () => {
+    dispatch({
+      type: "REMOVE_FROM_CART",
+      payload: product,
+    });
+  };
 
   return (
     <article className="border-2 border-gray-300 p-4 h-150">
@@ -14,9 +30,22 @@ const ProductsGrid = ({ product }) => {
       <p> {description}</p>
       <p className="font-bold"> {currencyFormatter(price)}</p>
       <div className="flex-col">
-        <button className="bg-blue-500 text-white border-none px-4 py-2 mt-4 cursor-pointer">
-          Add to Cart
-        </button>
+        {!state.cart.find((e) => e.id === product.id) && (
+          <button
+            className="bg-blue-500 text-white border-none px-4 py-2 mt-4 cursor-pointer"
+            onClick={addToCart}
+          >
+            Add to Cart
+          </button>
+        )}
+        {state.cart.find((e) => e.id == product.id) && (
+          <button
+            className="bg-red-500 text-white py-2 px-4 rounded"
+            onClick={removeFromCart}
+          >
+            Remove from Cart
+          </button>
+        )}
       </div>
     </article>
   );
--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 import { currencyFormatter } from "../../helpers/dataTransformations";
 import { Link } from "react-router-dom";
